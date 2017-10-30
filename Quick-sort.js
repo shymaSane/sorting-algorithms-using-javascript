e6f60a76ea74bf791e6870034d80931b15378e01
shymaSane <shymaSane1@gmail.com>
2017-10-30 10:45:55 +0200

    working on quick sort

---
+++ Quick-sort.js
@@ -0,0 +1,12 @@
+// O(n2) worst case / mid case nlog(n)
+
+var array = [7,4,2,8,1,23,45,18,3,90,23,1,67,4]
+
+function quickSort(array){
+    for(var i = 0; i < array.length; i++){
+        var x = array.length-1
+        var pviot = array[x]
+        var current = array[0]
+        for(var j =
+    }
+}
